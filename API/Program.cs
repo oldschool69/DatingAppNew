c5c259e72cc09e317861eda7c9fd196b87fadabf
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-10-14 16:07:01 -0300

    Criar tabela User no banco de dados

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,12 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
