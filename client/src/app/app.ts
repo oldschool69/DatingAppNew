b80ee80a94c2161f19367d60735d8f98d8d60f9f
Oldschool69 <flavio.marcondes@gmail.com>
2025-10-17 14:56:51 -0300

    remover conexao https em dev

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -12,14 +12,14 @@ export class App implements OnInit {
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
   protected members = signal<any>([]);
-  
+
   async ngOnInit() {
     this.members.set(await this.getMembers());
   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+      return lastValueFrom(this.http.get('http://localhost:5001/api/members'));
     } catch (error) {
       console.log(error);
       throw error;
