1c9136568b5feb6f894387c0811baae7301e54a0
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-04 18:18:57 -0300

    Chamando endpoint de autenticacao do front

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+
+  private accountService = inject(AccountService);
+  protected creds: any = {};
+
+  async login(){
+    try {
+      const result = await firstValueFrom(this.accountService.login(this.creds));
+      console.log(result);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  }
 
 }
