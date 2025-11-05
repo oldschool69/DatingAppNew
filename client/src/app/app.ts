d18c728e4549885559d94a45ddb86daa4eed7e57
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-11-05 11:53:09 -0300

    passando o array de membros para o componente filho

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -4,6 +4,7 @@ import { last, lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
 import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class App implements OnInit {
   private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
@@ -32,7 +33,7 @@ export class App implements OnInit {
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('http://localhost:5001/api/members'));
+      return lastValueFrom(this.http.get<User[]>('http://localhost:5001/api/members'));
     } catch (error) {
       console.log(error);
       throw error;
