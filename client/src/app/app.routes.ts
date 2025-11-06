b74c387621d9a9c34706c217fd902c55ae5f5dd7
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-11-06 10:42:25 -0300

    refactor: update navigation and routing structure

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Home } from '../features/home/home';
 import { MemberList } from '../features/members/member-list/member-list';
 import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,18 @@ export const routes: Routes = [
   {
     path: 'members/:id',
     component: MemberDetailed
-  }
+  },
+  {
+    path: 'lists',
+    component: Lists
+  },
+  {
+    path: 'messages',
+    component: Messages
+  },
+  {
+    path: '**',
+    component: Home
+  },
 
 ];
