aeccdb543116fa64b4ca5a4b229efb3591cf97e2
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-06 03:57:55 -0300

    criando novos componentes e rotas

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'members',
+    component: MemberList
+  },
+  {
+    path: 'members/:id',
+    component: MemberDetailed
+  }
+
+];
