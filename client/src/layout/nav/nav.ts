24f84beb34e4acfad8c02eb8cf69185fd875b08a
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-11-06 14:58:43 -0300

    feat: implement ToastService for user notifications and enhance login feedback

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,7 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { firstValueFrom } from 'rxjs';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -14,19 +15,24 @@ export class Nav {
 
   protected accountService = inject(AccountService);
   protected creds: any = {};
+  private router = inject(Router);
+  private toast = inject(ToastService);
 
   async login(){
     try {
-      const result = await firstValueFrom(this.accountService.login(this.creds));
-      console.log(result);
+      await firstValueFrom(this.accountService.login(this.creds));
+      this.router.navigateByUrl('/members');
+      this.toast.sucess('Login successful');
       this.creds = {};
     } catch (error: any) {
-      alert(error.message);
+      console.log('***DEBUG error ',error);
+      this.toast.error('Login failed: ' + error.error);
     }
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
