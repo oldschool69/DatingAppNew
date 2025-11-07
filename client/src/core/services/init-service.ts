f2c211d6ccb32f5f5f094776d8929ecb1deab927
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-07 10:13:02 -0300

    feat: implement initialization service and enhance app configuration with splash screen

---
+++ client/src/core/services/init-service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { AccountService } from './account-service';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitService {
+  private accountService = inject(AccountService);
+
+  init() {
+    const userJson = localStorage.getItem('user');
+    if (userJson) {
+      const user = JSON.parse(userJson);
+      this.accountService.currentUser.set(user);
+    }
+    return of(null);
+  }
+}
