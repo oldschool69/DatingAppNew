819549ee1008d0946dc8af8caaf5113cd654aefa
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-12 17:56:34 -0300

    feat: implement database seeding and migration handling in Program.cs

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -23,10 +23,11 @@ public class Seed
             return;
         }
 
-        using var hmac = new HMACSHA512();
 
         foreach (var member in members)
         {
+            using var hmac = new HMACSHA512();
+            
             var user = new AppUser
             {
                 Id = member.Id,
--- API/Program.cs
+++ API/Program.cs
@@ -42,4 +42,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+// Seed the database, run all migrations and create the database if it does not exist
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
