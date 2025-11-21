5db9a634819c73fb76a01aa0729b33e9cc0480cb
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-21 17:10:38 -0300

    feat: enhance image upload component with drag-and-drop functionality and loading state management

--- client/src/shared/image-upload/image-upload.ts
+++ client/src/shared/image-upload/image-upload.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-image-upload',
@@ -7,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './image-upload.css'
 })
 export class ImageUpload {
+  protected imageSrc = signal<string | ArrayBuffer | null>(null);
+  protected isDragging = false
+  private fileToUpload: File | null = null;
+  uploadFile = output<File>();
+  loading = input<boolean>(false);
 
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragging = true;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragging = false;
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragging = false;
+
+    if (event.dataTransfer?.files && event.dataTransfer.files.length > 0) {
+      const file = event.dataTransfer.files[0];
+      this.previewImage(file);
+      this.fileToUpload = file;
+    }
+  }
+
+  onCancel() {
+    this.imageSrc.set(null);
+    this.fileToUpload = null;
+  }
+
+  onUpload() {
+    if (this.fileToUpload) {
+      this.uploadFile.emit(this.fileToUpload);
+    }
+  }
+
+  private previewImage(file: File) {
+    const reader = new FileReader();
+    reader.onload = e => this.imageSrc.set(e.target?.result ?? null);
+    reader.readAsDataURL(file);
+  }
 }
