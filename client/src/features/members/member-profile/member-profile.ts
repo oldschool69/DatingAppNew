2e8788f8ed0091e3f52f87207681250eda94ebdd
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-21 09:39:44 -0300

    feat: add prevent unsaved changes guard and implement profile editing functionality

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,19 +1,35 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, HostListener, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../../types/member';
+import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
 import { MemberService } from '../../../core/services/member-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile implements OnInit {
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event: BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
+      $event.preventDefault();
+    }
+  }
   private route = inject(ActivatedRoute);
+  private toast = inject(ToastService);
   protected member = signal<Member | undefined>(undefined);
   protected memberService = inject(MemberService);
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  };
+
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe({
@@ -21,6 +37,32 @@ export class MemberProfile implements OnInit {
         this.member.set(data['member']);
       }
     });
+
+    this.editableMember = {
+      displayName: this.member()?.displayName || '',
+      description: this.member()?.description || '',
+      city: this.member()?.city || '',
+      country: this.member()?.country || ''
+    };
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.memberService.editMode()) {
+      this.memberService.editMode.set(false);
+    }
+  }
+
+  updateProfile() {
+    if (!this.member()) return;
+    const updatedMember = {
+      ...this.member(),
+      ...this.editableMember
+    };
+
+    console.log('***DEBUG updatedMember ', updatedMember);
+    this.toast.sucess('Profile updated successfully');
+    this.memberService.editMode.set(false);
   }
 
 }
