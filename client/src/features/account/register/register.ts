0e008e0ac48377147faca3360e3e8d4d2fc3ebd8
Oldschool69 <flavio.marcondes@gmail.com>
2025-11-25 18:24:36 -0300

    feat: enhance registration form with reactive validation and confirm password functionality

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,29 +1,58 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RegisterCreds, User } from '../../../types/user';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, Form, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { RegisterCreds } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit {
   private accountService = inject(AccountService);
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      displayName: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+      if (!parent) return null;
+
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : { passwordMismatch: true };
+    }
+  }
 
   register() {
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log('Registration successful:', response);
-        this.cancel();
-      },
-      error: (error: any) => {
-        console.error('Registration failed:', error);
-      }
-    });
+    console.log('***DEBUG ', this.registerForm.value);
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log('Registration successful:', response);
+    //     this.cancel();
+    //   },
+    //   error: (error: any) => {
+    //     console.error('Registration failed:', error);
+    //   }
+    // });
   }
 
   cancel() {
