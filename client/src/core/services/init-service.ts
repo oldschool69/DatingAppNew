b1dd6cfeebb6d7fbd36754e578a44d959cfff48e
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-12-10 11:50:22 -0300

    feat: integrate LikesService into AccountService and InitService; enhance member card with like functionality

--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,18 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { of } from 'rxjs';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
   private accountService = inject(AccountService);
+  private likesService = inject(LikesService);
 
   init() {
     const userJson = localStorage.getItem('user');
     if (userJson) {
       const user = JSON.parse(userJson);
       this.accountService.currentUser.set(user);
+      this.likesService.getLikeIds();
     }
     return of(null);
   }
