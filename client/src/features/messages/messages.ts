6b92317cf3f982cd2c860e2f934559cd4cbce96f
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-12-17 10:13:18 -0300

    feat: update messages component layout and functionality; enhance message display with images and adjust column widths, add RouterLink and DatePipe for improved navigation and date formatting

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -3,16 +3,19 @@ import { MessageService } from '../../core/services/message-service';
 import { PaginatedResult } from '../../types/pagination';
 import { Message } from '../../types/message';
 import { Paginator } from "../../shared/paginator/paginator";
+import { RouterLink } from "@angular/router";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages',
-  imports: [Paginator],
+  imports: [Paginator, RouterLink, DatePipe],
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
 export class Messages {
   private messageService = inject(MessageService);
   protected container = 'Inbox';
+  protected fetchedContainer = 'Inbox';
   protected pageNumber = 1;
   protected pageSize = 10;
   protected paginatedMessages = signal<PaginatedResult<Message> | null>(null);
@@ -30,11 +33,12 @@ export class Messages {
     this.messageService.getMessages(this.container, this.pageNumber, this.pageSize)
       .subscribe(response => {
         this.paginatedMessages.set(response);
+        this.fetchedContainer = this.container;
       });
   }
 
   get isInbox() {
-    return this.container === 'Inbox';
+    return this.fetchedContainer === 'Inbox';
   }
 
   setContainer(container: string) {
