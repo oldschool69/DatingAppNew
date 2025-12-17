7859695f7807aeae49a1d19793d074aaa9e212f5
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-12-17 11:36:53 -0300

    feat: implement messaging functionality; add sendMessage method to MessageService, update MemberMessages component for message sending and auto-scroll, and enhance message input UI

--- client/src/features/members/member-messages/member-messages.ts
+++ client/src/features/members/member-messages/member-messages.ts
@@ -1,20 +1,32 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MessageService } from '../../../core/services/message-service';
 import { MemberService } from '../../../core/services/member-service';
 import { Message } from '../../../types/message';
 import { DatePipe } from '@angular/common';
 import { TimeAgoPipe } from '../../../core/pipes/time-ago-pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
-  imports: [DatePipe, TimeAgoPipe],
+  imports: [DatePipe, TimeAgoPipe, FormsModule],
   templateUrl: './member-messages.html',
   styleUrl: './member-messages.css'
 })
 export class MemberMessages implements OnInit {
+  @ViewChild('messageEndRef') messageEndRef!: ElementRef;
   private messageService = inject(MessageService);
   private memberService = inject(MemberService);
   protected messages = signal<Message[]>([]);
+  protected messageContent = '';
+
+  constructor() {
+    effect(() => {
+      const currentMessages = this.messages();
+      if (currentMessages.length > 0) {
+        this.scrollToBottom();
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.loadMessages();
@@ -35,6 +47,32 @@ export class MemberMessages implements OnInit {
         }
       });
     }
+  }
+
+  sendMessage() {
+    const recipientId = this.memberService.member()?.id;
+    if (!recipientId) return;
+    this.messageService.sendMessage(recipientId, this.messageContent).subscribe({
+      next: message => {
+        this.messages.update(messages => {
+          message.currentUserSender = true;
+          return [...messages, message];
+        });
+        this.messageContent = '';
+      },
+    });
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      try {
+        if (this.messageEndRef) {
+          this.messageEndRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      } catch (err) {
+        console.error('Scroll to bottom failed:', err);
+      }
+    });
   }
 
 }
