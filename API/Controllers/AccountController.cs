36d792568fdf556b64388c9f4b7894729983cad1
Flavio Marcondes de Oliveira <fmarcondes@ciandt.com>
2025-12-18 15:14:52 -0300

    feat: refactor AccountController to use UserManager for user registration and login; improve error handling for identity operations

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,22 +6,17 @@ using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+public class AccountController(UserManager<AppUser> userManager, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
-        if (await EmailExists(registerDto.Email))
-        {
-            return BadRequest("Email is already in use");
-        }
-
-        using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
@@ -38,8 +33,17 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
             }
         };
 
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("identity", error.Description);
+            }
+
+            return ValidationProblem();
+        }
 
         return user.ToUserDto(tokenService);
     }
@@ -47,17 +51,19 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+        var user = await userManager.FindByEmailAsync(loginDto.Email);
         if (user == null)
         {
             return Unauthorized("Invalid email");
         }
 
-        return user.ToUserDto(tokenService);
-    }
+        var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
 
-    private async Task<bool> EmailExists(string email)
-    {
-        return await context.Users.AnyAsync(x => x.Email!.ToLower() == email.ToLower());
+        if (!result)
+        {
+            return Unauthorized("Invalid password");
+        }
+
+        return user.ToUserDto(tokenService);
     }
 }
