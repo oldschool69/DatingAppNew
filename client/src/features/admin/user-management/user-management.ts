cf36d94d884d0515ef54b9a94e4437d986d9fbfc
Oldschool69 <flavio.marcondes@gmail.com>
2025-12-19 16:14:34 -0300

    feat: implement user and photo management components; add user role management functionality, update admin service for API integration, and enhance admin UI with role-based tab navigation

---
+++ client/src/features/admin/user-management/user-management.ts
@@ -0,0 +1,65 @@
+import { Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
+import { AdminService } from '../../../core/services/admin-service';
+import { User } from '../../../types/user';
+
+@Component({
+  selector: 'app-user-management',
+  imports: [],
+  templateUrl: './user-management.html',
+  styleUrl: './user-management.css'
+})
+export class UserManagement implements OnInit {
+  @ViewChild('rolesModal') rolesModal!: ElementRef<HTMLDialogElement>;
+  private adminService = inject(AdminService);
+  protected users = signal<User[]>([]);
+  protected availableRoles = ['Admin', 'Moderator', 'Member'];
+  protected selectedUser: User | null = null;
+
+  openRolesModal(user: User) {
+    this.selectedUser = user;
+    this.rolesModal.nativeElement.showModal();
+  }
+
+  closeRolesModal() {
+    this.rolesModal.nativeElement.close();
+    this.selectedUser = null;
+  }
+
+  updateRoleSelection(role: string, event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    if (this.selectedUser) {
+      if (checked && !this.selectedUser.roles.includes(role)) {
+        this.selectedUser.roles.push(role);
+      } else if (!checked && this.selectedUser.roles.includes(role)) {
+        this.selectedUser.roles = this.selectedUser.roles.filter(r => r !== role);
+      }
+    }
+  }
+
+  updateUserRoles() {
+    if (this.selectedUser) {
+      const roles = this.selectedUser.roles;
+      this.adminService.updateUserRoles(this.selectedUser.id, roles).subscribe({
+        next: updatedRoles => {
+          if (this.selectedUser) {
+            this.selectedUser.roles = updatedRoles;
+          }
+          this.closeRolesModal();
+        }
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminService.getUsersWithRoles().subscribe({
+      next: users => {
+        this.users.set(users);
+      }
+    });
+  }
+
+}
