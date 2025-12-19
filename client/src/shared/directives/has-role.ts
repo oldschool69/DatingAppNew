804070b1e2f90983d9f3f359edabc5d46115efef
Oldschool69 <flavio.marcondes@gmail.com>
2025-12-19 15:14:31 -0300

    feat: implement admin role access control; add adminGuard for route protection, create HasRole directive for role-based template rendering, and update navigation for role checks

---
+++ client/src/shared/directives/has-role.ts
@@ -0,0 +1,24 @@
+import { Directive, inject, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { AccountService } from '../../core/services/account-service';
+
+@Directive({
+  selector: '[appHasRole]'
+})
+export class HasRole implements OnInit {
+  @Input() appHasRole: string[] = [];
+  private accountService = inject(AccountService);
+  private viewContainer = inject(ViewContainerRef);
+  private templateRef = inject(TemplateRef);
+
+  constructor() { }
+
+  ngOnInit() {
+    if (this.accountService.currentUser()?.roles.some(r => this.appHasRole.includes(r))) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
+
+
+}
